0bb9f1443fcf2bbebe92fa4bcb56302610faff8c
fadhilahrheza <61259935+fadhilahrheza@users.noreply.github.com>
2020-02-20 17:16:34 +0700

    Modul1
    Modul 1 Classes

---
+++ Invoice.java
@@ -0,0 +1,68 @@
+
+/**
+ * Write a description of class Invoice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Invoice
+{
+    private int id;
+    private int idFood;
+    private String date;
+    private int totalPrice;
+    
+    /**
+     * Constructor for objects of class Invoice
+     */
+    public Invoice()
+    {
+        
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int getId()
+    {
+        return 0;
+    }
+    
+    public int getIdFood()
+    {
+        return 0;
+    }
+    
+    public String getDate()
+    {
+        return "oke";
+    }
+    
+    public int getTotalPrice()
+    {
+        return 0;
+    }
+    
+    public void setId(int id)
+    {
+    
+    }
+    
+    public void setIdFoods(int Food)
+    {
+    
+    }
+    
+    public void setDate(String date)
+    {
+    
+    }
+    
+    public void seTotalPrice(int totalPrice)
+    {
+    
+    }
+}
