46c23d889ee59db1385e95d501235e86dcd157d5
fadhilahrheza <61259935+fadhilahrheza@users.noreply.github.com>
2020-02-27 17:45:21 +0700

    Modul 2
    Modul 2

--- JFood.java
+++ JFood.java
@@ -2,5 +2,10 @@ public class JFood
 {
     public static void main(String[] args)
     {
+        Location locObj = new Location("Jakarta", "Macet", "Jakarta");
+        locObj.printData();
+        
+        Seller selObj = new Seller(1, "Fred", "09-01-1990", "911", locObj);
+        selObj.printData();
     }
 }
